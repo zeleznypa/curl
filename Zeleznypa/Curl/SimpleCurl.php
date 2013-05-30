0e32c3eddc04aad3b43ecb4b9fa8b8e59c57966b
Pavel Zelezny <info@pavelzelezny.cz>
2013-05-30 10:54:44 +0200

    First form of core method execute()

--- Zeleznypa/Curl/SimpleCurl.php
+++ Zeleznypa/Curl/SimpleCurl.php
@@ -9,4 +9,21 @@ namespace Zeleznypa\Curl;
 class SimpleCurl
 {
 
+	/**
+	 * Execute cURL request with defined params
+	 * @author Pavel Železný <info@pavelzelezny.cz>
+	 * @param string $url
+	 * @param array $options
+	 * @return string|bool Depend on option CURLOPT_RETURNTRANSFER
+	 */
+	public function execute($url, array $options = array())
+	{
+		$ch = curl_init($url);
+		curl_setopt_array($ch, $options);
+		$result = curl_exec($ch);
+		curl_close($ch);
+
+		return $result;
+	}
+
 }
